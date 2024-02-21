22b663d5c319339f2e2cca769382746bdde1a0e1
Ayush Singh <142167916+ayush-singh09@users.noreply.github.com>
2024-02-21 12:07:47 +0530

    Update Songs.jsx

--- src/Components/Songs.jsx
+++ src/Components/Songs.jsx
@@ -4,10 +4,9 @@ import { myContext } from "./Context";
 
 function Songs() {
   const {songs} = useContext(myContext);
-  const song= songs[0];
   return (
     <div className="songs h-full w-[74%] bg-zinc-800 p-5 flex flex-wrap gap-5 justify-evenly rounded-lg overflow-y-scroll no-scrollbar">
-      {song.map((item, index) => (
+      {songs.map((item, index) => (
         <SongCard key={index} data={item} />
       ))}
     </div>
