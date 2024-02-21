f15154c06b919f2d8e71749972c6900de3f42f90
Ayush Singh <142167916+ayush-singh09@users.noreply.github.com>
2024-02-21 12:28:05 +0530

    Update Tab.jsx

--- src/Components/Tab.jsx
+++ src/Components/Tab.jsx
@@ -6,17 +6,8 @@ function Tab({ data }) {
   const { playlists, setPlaylists } = useContext(myContext);
   const navigate = useNavigate();
 
-  function toCamelCase(words) {
-    return words
-      .toLowerCase()
-      .replace(/(?:^\w|[A-Z]|\b\w)/g, function (word, index) {
-        return index === 0 ? word.toLowerCase() : word.toUpperCase();
-      })
-      .replace(/\s+/g, "");
-  }
-
   const handlePlaylistclick = () => {
-    const navigatePage = "/playlist/" + toCamelCase(data.playlistName);
+    const navigatePage = "/playlist/" + data.playlistName;
     navigate(navigatePage);
   };
 
